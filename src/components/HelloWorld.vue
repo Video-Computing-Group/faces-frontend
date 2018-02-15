970388c5bd9d81f2f8142ac11a5d89a39e84cd4e
John Pham <johnphammail@gmail.com>
2018-02-14 16:42:38 -0800

    finishing part 1 of front end

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,69 +1,126 @@
 <template>
   <div id="main">
     <div class="row img-container">
-      <div class="col-md-12 text-center">
+      <div class="col-md-12 d-flex justify-content-center align-items-center">
         <h1>Faces</h1>
       </div>
 
-      <div class="img-upload col-md-6">
-        <div class="card">
-          <div class="card-body">
-            <img v-if="img1"
-              :src="img1"
-              class="userImage"
-            />
+      <div class="img-upload row">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <img v-if="img1"
+                :src="img1"
+                class="userImage"
+              />
 
-            <div v-if="!img1" class="dropbox">
-              <input type="file" :name="img1" @change="onFileChange" accept="image/*" class="input-file">
-              <p>
-                Drag your file(s) here to begin<br> or click to browse
-              </p>
+              <div v-if="!img1" class="dropbox">
+                <input type="file" :name="img1" @change="onFileChange1" accept="image/*" class="input-file">
+                <p>
+                  Drag your file(s) here to begin<br> or click to browse
+                </p>
+              </div>
+
+              <button
+                v-if="img1"
+                @click="removeImage(1)"
+                class="btn btn-primary btn-remove"
+              >Remove Image 1</button>
             </div>
+          </div>
+        </div>
 
-            <button
-              v-if="img1"
-              @click="removeImage(1)"
-              class="btn btn-primary btn-remove"
-            >Remove Image 1</button>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <img v-if="img2"
+                :src="img2"
+                class="userImage"
+              />
+
+              <div v-if="!img2" class="dropbox">
+                <input type="file" :name="img2" @change="onFileChange2" accept="image/*" class="input-file">
+                <p>
+                  Drag your file(s) here to begin<br> or click to browse
+                </p>
+              </div>
+
+              <button
+                v-if="img2"
+                @click="removeImage(2)"
+                class="btn btn-primary btn-remove"
+              >Remove Image 2</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 offset-md-3" v-if="comparing" >
+          <div class="card data">
+            <div class="my-auto card-body text-center">
+              <h1>Data: 100%</h1>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="img-upload col-md-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Image 2</h5>
-          </div>
-        </div>
-      </div>
 
-      <button type="button" class="btn btn-primary btn-lg btn-block">Compare Images</button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-block"
+        @click="compareImages"
+        v-if="!comparing"
+      >
+        Compare Images
+      </button>
+
+      <button
+        type="button"
+        class="btn btn-warning btn-lg btn-block"
+        @click="reset"
+        v-else
+      >
+        Compare New Images
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import swal from 'sweetalert2'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       img1: '',
-      img2: ''
+      img2: '',
+      comparing: false
     }
   },
   methods: {
-    onFileChange(e) {
+    onFileChange1(e) {
       let files = e.target.files || e.dataTransfer.files;
       if (!files.length)
         return;
-      this.createImage(files[0]);
+      this.createImage(files[0], 1);
     },
-    createImage(file) {
+    onFileChange2(e) {
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0], 2);
+    },
+    createImage(file, num) {
       let image = new Image();
       let reader = new FileReader();
 
       reader.onload = (e) => {
-        this.img1 = e.target.result;
+        if (num === 1) {
+          this.img1 = e.target.result;
+        }
+        else if (num === 2) {
+          this.img2 = e.target.result;
+        }
       };
       reader.readAsDataURL(file);
     },
@@ -74,19 +131,35 @@ export default {
       else if (num === 2) {
         this.img2 = '';
       }
+    },
+    compareImages() {
+      if (!this.img1.length || !this.img2.length) {
+        swal(
+          'Error',
+          'You must upload 2 images',
+          'error'
+        )
+      }
+      else {
+        this.comparing = true;
+      }
+
+    },
+    reset() {
+      this.comparing = false;
+      this.img1 = '';
+      this.img2 = '';
     }
   }
 }
 </script>
 
 <style>
-html, body, #main {
-  height: 100%;
+
+html, body, #main, .swal2-shown {
+  height: 100% !important;
   width: 100%;
   background: #f4f4f4;
-}
-.card {
-  height: 100%;
 }
 .img-container {
   height: 100%;
@@ -102,7 +175,7 @@ html, body, #main {
   outline: 2px dashed grey; /* the dash box */
   background: lightcyan;
   color: dimgray;
-  height: 90%;
+  height: 100%;
   width: 100%;
   position: relative;
   cursor: pointer;
@@ -131,5 +204,13 @@ html, body, #main {
 .btn-remove {
   bottom: 20px;
   position: absolute;
+}
+.row {
+  margin-left: auto;
+  margin-right: auto;
+}
+.data-text {
+  top: 40%;
+  position: relative;
 }
 </style>
